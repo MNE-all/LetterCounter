2a9d8231460b1d2194645e297c6bd2e42cfb5dff
MNE4 <monashovnikolaye@gmail.com>
2023-01-24 21:27:26 +0300

    Code optimization

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,18 +3,9 @@ import java.util.*
 fun main() {
     println("Программа для подсчёта букв в строке")
     print("Введите строку: ")
-    val str = readln()
-
-    var letters: MutableList<Char> = mutableListOf(str[0])
-
-    for (element in str){
-        if (!letters.contains(element)){
-            letters.add(element)
-        }
-    }
-    letters.sort()
-
-    for (item in letters){
-        println(item + ": " + Collections.frequency(str.toList(), item))
+    val userInput = readln()
+    val letters = userInput.toSortedSet()
+    for (letter in letters){
+        println(letter + ": " + Collections.frequency(userInput.toList(), letter))
     }
 }
